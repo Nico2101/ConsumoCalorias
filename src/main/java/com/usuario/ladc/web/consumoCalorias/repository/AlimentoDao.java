8b9bb873bad5b3ddd7f8df429ac2e2fae4db9e7b
daniiGallegos <danielagallegosbaeza@gmail.com>
2017-06-28 14:34:46 -0400

    Agregar RegistroUsuario - Corrección #5

--- src/main/java/com/usuario/ladc/web/consumoCalorias/repository/AlimentoDao.java
+++ src/main/java/com/usuario/ladc/web/consumoCalorias/repository/AlimentoDao.java
@@ -26,7 +26,7 @@ public class AlimentoDao {
     @Transactional(readOnly = true)
     @SuppressWarnings("unchecked")
     public List<Alimento> getListaAlimentos(Integer usuario) {
-        return em.createQuery("from Alimento a where a.id_usuario= '"+usuario+"'a order by a.id").getResultList();
+        return em.createQuery("from Alimento a where a.id_usuario= '"+usuario+"' a order by a.id").getResultList();
     }
     
     @Transactional(readOnly = true)
